9865bc0cfe86cf7abbba8ed499de85fd1aa976df
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:10:14 +0300

    Create PeerDataViewModel.cs

---
+++ ClientDesktopUI/ViewModels/PeerDataViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientDesktopUI.ViewModels
+{
+    public class PeerDataViewModel : INotifyPropertyChanged
+    {
+        private string _PeerName;
+        private bool _IsAvailable;
+
+        private ObservableCollection<ChatMessageViewModel> _ChatMessages = new ObservableCollection<ChatMessageViewModel>();
+
+        public string PeerName
+        {
+            get => _PeerName; set
+            {
+                _PeerName = value;
+                onPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ChatMessageViewModel> ChatMessages
+        {
+            get => _ChatMessages; set
+            {
+                _ChatMessages = value;
+                onPropertyChanged();
+            }
+        }
+
+        public bool IsAvailable
+        {
+            get => _IsAvailable; set
+            {
+                _IsAvailable = value;
+                onPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void onPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public PeerDataViewModel(string peerName, bool isAvailable = true)
+        {
+            PeerName = peerName;
+            IsAvailable = isAvailable;
+        }
+    }
+}
