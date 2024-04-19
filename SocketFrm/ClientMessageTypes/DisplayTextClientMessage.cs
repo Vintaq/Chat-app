6ff6dfdc4f038cc47cfddbb0fc4c80fb266b48b4
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:29:31 +0300

    Create DisplayTextClientMessage.cs

---
+++ SocketFrm/ClientMessageTypes/DisplayTextClientMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketFrm
+{
+    [Serializable]
+    [DataContract]
+    public class DisplayTextClientMessage : ClientMessage
+    {
+        [DataMember]
+        private string _DisplayText;
+
+        public DisplayTextClientMessage(string displayText)
+        {
+            _DisplayText = displayText;
+            ClientMessageType = ClientMessageType.DisplayTextToConsole;
+        }
+
+        
+        public string DisplayText { get => _DisplayText; set => _DisplayText = value; }
+    }
+}
