90cb94c7c35b23229818efd229a0db78eb815974
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:40:40 +0300

    Create IServerUINotifier.cs

---
+++ SocketFrm/ServerMessageTypes/IServerUINotifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SocketFrm.ClientMessageTypes;
+
+namespace SocketFrm
+{
+    public interface IServerUINotifier
+    {
+        ManualResetEvent ServerWantsShutdown { get; }
+
+        void HandleDisplayTextToConsoleMessage(DisplayTextClientMessage displayTextClientMessage);
+        void HandleRegisterIDInServerMessage(RegisterIdClientMessage registerIdClientMessage);
+        void HandleTransmitToPeerMessage(TransmitToPeerClientMessage transmitToPeerClientMessage);
+        void NotifyClientDisconnection(string clientDisconnectedID);
+        void LogException(Exception exception, string exceptionMessage);
+        void LogText(string logMessage);
+        void HandleSignInServerMessage(SignInClientMessage signInClientMessage);
+    }
+}
