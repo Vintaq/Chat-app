27d96860275aa7705234c804b5e764d794708ed3
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:39:24 +0300

    Create IClientUINotifier.cs

---
+++ SocketFrm/ServerMessageTypes/IClientUINotifier.cs
@@ -0,0 +1,22 @@
+﻿using SocketFrm.ServerMessageTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketFrm
+{
+    public interface IClientUINotifier
+    {
+        ManualResetEvent ClientWantsShutdown { get; }
+
+        void HandleDisplayTextServerMessage(DisplayTextServerMessage displayTextServerMessage);
+        void HandleRegisterIDResultServerMessage(RegisterIdResultServerMessage registerIdResultServerMessage, IPeerMessageTransmitter clientAppState);
+        void HandleSignInResultServerMessage(SignInResultServerMessage signInResultServerMessage, IPeerMessageTransmitter clientAppState);
+        void HandleClientAvailabilityNotificationServerMessage(ClientAvailabilityNotificationServerMessage clientAvailabilityNotificationServerMessage);
+        void HandleTransmitToPeerResultServerMessage(TransmitToPeerResultServerMessage transmitToPeerResultServerMessage);
+        void HandleTransmitToPeerServerMessage(TransmitToPeerServerMessage transmitToPeerServerMessage);
+    }
+}
