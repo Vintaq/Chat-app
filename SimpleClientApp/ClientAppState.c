a4bcb1ad3cb59be939dd21c177ca145dbb00e75d
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:25:55 +0300

    Create ClientAppState.c

---
+++ SimpleClientApp/ClientAppState.c
@@ -0,0 +1,25 @@
+﻿using SocketFrm;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClientApp
+{
+    internal class ClientAppState : IPeerMessageTransmitter
+    {
+        private ConcurrentQueue<ClientMessage> _clientMessagesQueue = new ConcurrentQueue<ClientMessage>();
+
+        internal ConcurrentQueue<ClientMessage> ClientMessagesQueue { get => _clientMessagesQueue; set => _clientMessagesQueue = value; }
+        
+        internal TcpClient TCPClient { get; set; }
+
+        public void QueueToTransmitPeerMessage(TransmitToPeerClientMessage clientMessage)
+        {
+            ClientMessagesQueue.Enqueue(clientMessage);
+        }
+    }
+}
