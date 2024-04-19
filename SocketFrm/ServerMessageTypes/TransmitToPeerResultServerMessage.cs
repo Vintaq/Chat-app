401b6323c0d1109ff4e2aecf1b2206d90304f4e9
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:38:22 +0300

    Create TransmitToPeerResultServerMessage.cs

---
+++ SocketFrm/ServerMessageTypes/TransmitToPeerResultServerMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketFrm
+{
+    [Serializable]
+    [DataContract]
+    public class TransmitToPeerResultServerMessage : ServerMessage
+    {
+        [DataMember]
+        private string _ReceiverClientId;
+        [DataMember]
+        private int _SenderMessageId;
+        [DataMember]
+        private bool _IsReceivedByReceiver;
+
+        public TransmitToPeerResultServerMessage(string receiverClientId, int senderMessageId, bool isReceivedByReceiver)
+        {
+            ReceiverClientId = receiverClientId;
+            SenderMessageId = senderMessageId;
+            IsReceivedByReceiver = isReceivedByReceiver;
+            ServerMessageType = ServerMessageType.TransmitToPeerResult;
+        }
+
+        public string ReceiverClientId { get => _ReceiverClientId; private set => _ReceiverClientId = value; }
+        public int SenderMessageId { get => _SenderMessageId; private set => _SenderMessageId = value; }
+        public bool IsReceivedByReceiver { get => _IsReceivedByReceiver; private set => _IsReceivedByReceiver = value; }
+    }
+}
