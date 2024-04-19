4656a11ad90ab3e829a3e4af249d6c7022c00ad2
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:43:51 +0300

    Create IAuthenticationService.cs

---
+++ SocketServerApp/Authentication/IAuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServerApp.Authentication
+{
+    public interface IAuthenticationService
+    {
+        bool SignIn(string userEmail, string password, out SignInErrorCode signInErrorCode);
+
+        bool SignUp(string userEmail, string password, out SignUpErrorCode signUpErrorCode);
+
+        string Name { get; }
+    }
+
+    public enum SignInErrorCode { NoError, InvalidCredentials, UnknownError }
+
+    public enum SignUpErrorCode { NoError, UserEmailAlreadyExists, UnknownError }
+}
