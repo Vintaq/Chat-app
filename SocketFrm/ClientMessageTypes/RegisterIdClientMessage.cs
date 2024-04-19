2ec61f08978f5cbeae66f8c92e2a9bdad1751f8f
Vintage <167544574+Vintaq@users.noreply.github.com>
2024-04-19 23:30:06 +0300

    Create RegisterIdClientMessage.cs

---
+++ SocketFrm/ClientMessageTypes/RegisterIdClientMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketFrm
+{
+    [Serializable]
+    [DataContract]
+    public class RegisterIdClientMessage : ClientMessage
+    {
+        [DataMember]
+        private string _emailId;
+
+        [DataMember]
+        private string _password;
+
+        public RegisterIdClientMessage(string emailId, string password)
+        {
+            EmailId = emailId;
+            Password = password;
+            ClientMessageType = ClientMessageType.RegisterIDInServer;
+        }
+
+        public string EmailId { get => _emailId; private set => _emailId = value; }
+        public string Password { get => _password; private set => _password = value; }
+    }
+}
